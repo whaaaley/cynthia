ee201a42938ef1d53a1d7cd6a11b992a86acc594
Dustin Dowell <7419947+whaaaley@users.noreply.github.com>
2025-06-26 21:21:00 -0500

    Fix imports for jsr

--- src/core/cynthia.test.ts
+++ src/core/cynthia.test.ts
@@ -1,5 +1,5 @@
-import { assertEquals, assertExists } from 'jsr:@std/assert'
-import { describe, it } from 'jsr:@std/testing/bdd'
+import { assertEquals, assertExists } from '@std/assert'
+import { describe, it } from '@std/testing/bdd'
 import { cynthia } from './cynthia.ts'
 
 // Mock modules to test with
